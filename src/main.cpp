4f33e3a565e0d7326172ded2afd8e4d7a66f1121
James Pallister <james.pallister@bristol.ac.uk>
2015-06-12 11:00:31 +0100

    Test if the instructions are equal to some function

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,16 @@ int main(int argc, char *argv[])
 
     // Initialise backend
 
-    AvrMachine mach;
+    AvrMachine mach, mach_expected;
+    uint8_t vals[32];
+
+    for(int i = 0; i < 32; ++i)
+    {
+        vals[i] = rand() & 0xFF;
+        mach_expected.setRegisterValue(i, vals[i]);
+    }
+
+    mach_expected.setRegisterValue(0, mach_expected.getRegisterValue(0)*4+1);
 
     // Get a list of functions which will construct an instruction
     auto insn_factories = mach.getInstructionFactories();
@@ -47,7 +56,6 @@ int main(int argc, char *argv[])
         {
             auto va = static_cast<RegisterSlot*>(slot)->getValidArguments();
 
-            cout << *va.begin() << endl;
             slot->setValue(*min_element(va.begin(), va.end()));
         }
 
@@ -56,22 +64,66 @@ int main(int argc, char *argv[])
         do
         {
             for(int i = 0; i < 32; ++i)
-                mach.setRegisterValue(i, i+1);
+                mach.setRegisterValue(i, vals[i]);
 
             auto current_slot = &slots[0];
 
             for(auto insn: insns)
             {
-                cout << insn->toString(current_slot) << endl;
-
                 auto n = insn->execute(&mach, current_slot);
                 current_slot += n;
             }
-            cout << endl;
+
+            if(mach == mach_expected)
+            {
+                AvrMachine mach_test;
+
+                bool correct = true;
+                int i;
+                for(i = 0; i < 100; ++i)
+                {
+                    for(int j = 0; j < 32; ++j)
+                    {
+                        uint8_t v = rand() & 0xFF;
+                        mach.setRegisterValue(j, v);
+                        mach_test.setRegisterValue(j, v);
+                    }
+                    mach_test.setRegisterValue(0, mach_test.getRegisterValue(0)*4+1);
+
+                    auto current_slot = &slots[0];
+
+                    for(auto insn: insns)
+                    {
+                        auto n = insn->execute(&mach, current_slot);
+                        current_slot += n;
+                    }
+
+                    if(!(mach == mach_test))
+                    {
+                        correct = false;
+                        break;
+                    }
+                }
+
+                if(correct)
+                {
+                    cout << "Found" << endl;
+                    auto current_slot = &slots[0];
+
+                    for(auto insn: insns)
+                    {
+                        cout << "    " << insn->toString(current_slot) << endl;
+
+                        auto n = insn->execute(&mach, current_slot);
+                        current_slot += n;
+                    }
+                    cout << endl;
+                    // return 0;
+                }
+            }
+            // break;
         } while(nextCanonical(slots, canonical_skips));
 
-
-        cout << endl;
     } while(bruteforceIterate(insn_factories, current_factories));
 
 
