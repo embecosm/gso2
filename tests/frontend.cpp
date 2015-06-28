ded6d2f039a2ffae4a32a1976d08207d9a42459a
James Pallister <james.pallister@bristol.ac.uk>
2015-06-28 14:33:52 +0100

    Added frontend test file

---
+++ tests/frontend.cpp
@@ -0,0 +1,50 @@
+#define BOOST_TEST_MODULE frontend test
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "frontend.hpp"
+
+BOOST_AUTO_TEST_CASE( target_machine_register_test )
+{
+    TargetMachine<uint32_t, 8> test_mach, expected_mach;
+
+    for(int i = 0; i < 8; ++i)
+        BOOST_CHECK(test_mach.getRegisterValue(i) == 0);
+
+    BOOST_CHECK(test_mach.equivalentState(expected_mach) == true);
+
+    test_mach.setRegisterValue(0, 0xFFFFFFFF);
+    BOOST_CHECK(test_mach.getRegisterValue(0) == 0xFFFFFFFF);
+    for(int i = 1; i < 8; ++i)
+        BOOST_CHECK(test_mach.getRegisterValue(i) == 0);
+
+    BOOST_CHECK(test_mach.equivalentState(expected_mach) == false);
+}
+
+BOOST_AUTO_TEST_CASE( target_machine_containsState_test )
+{
+    TargetMachine<uint32_t, 8> test_mach, expected_mach;
+
+    expected_mach.setRegisterValue(0, 0xFFFFFFFF);
+    test_mach.setRegisterValue(0, 0xFFFFFFFF);
+
+    BOOST_CHECK(test_mach.containsState(expected_mach) == true);
+
+    test_mach.setRegisterValue(1, 0xFFFFFFFF);
+    BOOST_CHECK(test_mach.containsState(expected_mach) == true);
+
+    test_mach.setRegisterValue(0, 0x0);
+    BOOST_CHECK(test_mach.containsState(expected_mach) == true);
+
+    test_mach.setRegisterValue(1, 0x0);
+    BOOST_CHECK(test_mach.containsState(expected_mach) == false);
+
+    expected_mach.setRegisterValue(0, 0xF00DCAFE);
+    expected_mach.setRegisterValue(2, 0xBEEFCAFE);
+    test_mach.setRegisterValue(0, 0x1);
+    test_mach.setRegisterValue(1, 0x2);
+    test_mach.setRegisterValue(2, 0xF00DCAFE);
+    test_mach.setRegisterValue(3, 0x4);
+    test_mach.setRegisterValue(4, 0xBEEFCAFE);
+    BOOST_CHECK(test_mach.containsState(expected_mach) == true);
+}
