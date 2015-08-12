6e37624cae6934acbfe626428af99ae3c3d73f90
James Pallister <james.pallister@bristol.ac.uk>
2015-08-12 13:28:22 +0100

    Added comments for the test functions

--- src/algorithms/test.hpp
+++ src/algorithms/test.hpp
@@ -6,6 +6,18 @@
 #include "../frontend.hpp"
 #include "../slots.hpp"
 
+/*! Execute a sequence of instructions, with the specified slots.
+
+    This function simply iterates over the sequence of instructions, calling
+    the execute method of each instruction, and passing the appropriate slots.
+    In future, when branch instructions are implemented this will have to
+    execute the instructions in a smarter order.
+
+    @param insns    A list of the instructions to be executed.
+    @param slots    A corresponding list of slots which the instructions
+                    accept.
+    @param state    The state on which to execute the instructions.
+*/
 template <typename Machine>
 void executeSequence(std::vector<Instruction *> &insns, std::vector<Slot *> &slots,
     Machine &state)
@@ -54,6 +66,24 @@ bool testEquivalence(std::vector<Instruction *> &insns, std::vector<Slot *> &slo
     return test_state.containsState(expected, register_mapping);
 }
 
+/*! As with the previous version of testEquivalence, this tests a single test
+    vector, however it derives the test vector from an arbitrary
+    transformation function, and an initial state.
+
+    TODO maybe this function should accept a register_mapping parameter.
+
+    @param insns                The instruction sequence.
+    @param slots                The list of slots.
+    @param initial              The initial test vector.
+    @param transform            A function which takes a machine state, and
+                                modifies it.
+    @param register_mapping     A mapping between registers for the sequence
+                                under test. See TargetMachine::containsState
+                                for more information.
+    @return                     Is the state obtained when applying the
+                                instructions to the initial parameter
+                                contained within the expected state.
+*/
 template <typename Machine>
 bool testEquivalence(std::vector<Instruction *> &insns, std::vector<Slot *> &slots,
     Machine &initial, std::function<void(Machine&)> transform,
@@ -65,6 +95,25 @@ bool testEquivalence(std::vector<Instruction *> &insns, std::vector<Slot *> &slo
     return testEquivalence(insns, slots, initial, transformed, register_mapping);
 }
 
+/*! This form of testEquivalence takes two instruction sequences, and compares
+    them to each other. A register mapping parameter is also given, and passed
+    to containsState. This allows the function's use in testing multiple
+    times, while respecting the register assignments found. This avoids
+    problems where each random test vector can be matched in some way, but
+    requiring different register assignments.
+
+    @param insns                The instruction sequence under test.
+    @param slots                Slots for the instruction sequence under test.
+    @param reference_insns      The reference sequence (sequence to be
+                                matched).
+    @param reference_slots      The slots for the reference sequence.
+    @param register_mapping     A mapping between registers for the sequence
+                                under test. See TargetMachine::containsState
+                                for more information.
+    @return                     Is the state obtained when applying the
+                                instructions to the initial parameter
+                                contained within the expected state.
+*/
 template <typename Machine>
 bool testEquivalence(std::vector<Instruction *> &insns, std::vector<Slot *> &slots,
     std::vector<Instruction *> &reference_insns, std::vector<Slot *> &reference_slots,
@@ -77,6 +126,7 @@ bool testEquivalence(std::vector<Instruction *> &insns, std::vector<Slot *> &slo
     test_state.initialiseRandom();
     reference_state = test_state;
 
+    // Execute the sequence under test
     executeSequence(insns, slots, test_state);
 
     // Execute the reference sequence
@@ -86,6 +136,23 @@ bool testEquivalence(std::vector<Instruction *> &insns, std::vector<Slot *> &slo
 }
 
 
+/*! Test the equivalence of an instruction seqence with another instruction
+    sequence multiple times to see if they could be equivalent. Note that
+    parameters are not commutative: since it is possible that the instruction
+    sequence under test could produce unneccesary side effect, these can be
+    ignored and the sequence still considered equivalent.
+
+    @param insns                The instruction sequence under test.
+    @param slots                Slots for the instruction sequence under test.
+    @param reference_insns      The reference sequence (sequence to be
+                                matched).
+    @param reference_slots      The slots for the reference sequence.
+    @param number               The number of test vectors to apply to the
+                                sequences.
+    @return                     Is the state obtained when applying the
+                                instructions to the initial parameter
+                                contained within the expected state.
+*/
 template <typename Machine>
 bool testEquivalenceMultiple(std::vector<Instruction *> &insns,
     std::vector<Slot *> &slots, std::vector<Instruction *> reference_insns,
@@ -102,6 +169,23 @@ bool testEquivalenceMultiple(std::vector<Instruction *> &insns,
     return true;
 }
 
+/*! Test the equivalence of an instruction seqence with another instruction
+    sequence multiple times to see if they could be equivalent. Note that
+    parameters are not commutative: since it is possible that the instruction
+    sequence under test could produce unneccesary side effect, these can be
+    ignored and the sequence still considered equivalent.
+
+    @param insns                The instruction sequence under test.
+    @param slots                Slots for the instruction sequence under test.
+    @param reference_insns      The reference sequence (sequence to be
+                                matched).
+    @param reference_slots      The slots for the reference sequence.
+    @param number               The number of test vectors to apply to the
+                                sequences.
+    @return                     Is the state obtained when applying the
+                                instructions to the initial parameter
+                                contained within the expected state.
+*/
 template <typename Machine>
 bool testEquivalenceMultiple(std::vector<Instruction *> &insns,
     std::vector<Slot *> &slots, std::function<void(Machine&)> transform,
