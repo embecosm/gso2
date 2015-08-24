c896f32d7395cbfce06c1ef2f00781de280250cc
James Pallister <james.pallister@bristol.ac.uk>
2015-08-24 15:41:55 +0100

    Updated the parallel example superoptimizer

--- src/main_parallel.cpp
+++ src/main_parallel.cpp
@@ -6,8 +6,8 @@
 #include "parallel/mpi.hpp"
 
 #include "frontends/avr.hpp"
-// #include "frontends/x86_64.hpp"
 #include "algorithms/canonicalIterator.hpp"
+#include "algorithms/constantIterator.hpp"
 #include "algorithms/bruteforce.hpp"
 #include "algorithms/test.hpp"
 #include "utility.hpp"
@@ -23,24 +23,18 @@ int main(int argc, char *argv[])
 
     MPI_Init(NULL, NULL);
 
-    // An example goal function. Eventually we should read in a sequence of
-    // instructions and use this as a goal.
-    function<void(MachineType&)> goal(
-        [](MachineType &mach){
-            // mach.setRegisterValue(1, (mach.getRegisterValue(1)+37)&0xFF);
-            mach.setRegisterValue(0, 0x10);
-            mach.setRegisterValue(1, 0x11);
-            // mach.setRegisterValue(2, 0x12);
-            // mach.setRegisterValue(3, 0x13);
-        }
-    );
+   std::string instruction_str =
+        "add r0, r0\n"
+        "add r0, r0\n"
+        "add r0, r0\n"
+        "add r0, r0\n"
+        "add r0, r0";
 
     MachineType mach, mach_expected, mach_initial;
 
     // Compute a test state - for quick discarding of invalid sequences.
     mach_expected.initialiseRandom();
     mach_initial = mach_expected;
-    goal(mach_expected);
 
     // Get a list of functions which will construct an instruction
     auto insn_factories = mach.getInstructionFactories();
@@ -78,6 +72,19 @@ int main(int argc, char *argv[])
 
     cout << "SLAVE " << world_rank << endl;
 
+    // Get a list of instructions and slots from the string
+    vector<Instruction*> goal_insns;
+    vector<Slot*> goal_slots;
+
+    parseInstructionList(instruction_str, insn_factories, goal_insns, goal_slots);
+
+    // Create a quick test state
+    executeSequence(goal_insns, goal_slots, mach_expected);
+
+    cout << "Goal sequence:\n";
+    cout << print(goal_insns, goal_slots) << endl;
+
+
     vector<int> insn_ids;
 
     while(getWork(insn_ids)) {
@@ -88,7 +95,6 @@ int main(int argc, char *argv[])
             insns.push_back(insn_factories[insn_ids[i]]());
 
         vector<Slot*> slots;
-        vector<MachineType::ConstantSlot*> constant_slots;
 
         // Get a list of all of the slots in the instruction list
         for(auto insn: insns)
@@ -97,27 +103,10 @@ int main(int argc, char *argv[])
             slots.insert(slots.end(), s1.begin(), s1.end());
         }
 
-        // Work out what types of slots we need to iterate over
-        for(auto slot: slots)
-        {
-            if(dynamic_cast<RegisterSlot*>(slot) != 0)
-            {
-                auto va = static_cast<RegisterSlot*>(slot)->getValidArguments();
-                slot->setValue(*min_element(va.begin(), va.end()));
-            }
-            else if(dynamic_cast<MachineType::ConstantSlot*>(slot) != 0)
-            {
-                constant_slots.push_back((MachineType::ConstantSlot*)slot);
+        canonicalIteratorBasic c_iter(slots, 0);
 
-                ((MachineType::ConstantSlot*)slot)->iteratorSkip(true);
-                slot->setValue(0);
-            }
-            else
-                slot->setValue(0);
-        }
-
-        canonicalIteratorGeneric c_iter(slots);
-        // cout << print(insns,slots) << endl;
+        constantIterator cons_iter(slots);
+        cons_iter.setLossy(true);
 
         // Bruteforce over the possible constants, and canonically iterate
         // over the register slots.
@@ -125,25 +114,26 @@ int main(int argc, char *argv[])
         {
             do
             {
-                mach = mach_initial;
-
                 // Perform an equivalence test, and if it succeeds, run
                 // multiple tests to work out if the sequence is likely to be
                 // correct.
                 if(testEquivalence(insns, slots, mach_initial, mach_expected))
                 {
-                    MachineType mach_test;
-
-                    bool correct = testEquivalenceMultiple(insns, slots, goal);
+                    bool correct = testEquivalenceMultiple<AvrMachine>(insns, slots, goal_insns, goal_slots);
 
                     if(correct)
                     {
-                        cout << "Found" << endl;
-                        cout << print(insns, slots) << endl;
+                        auto mapping = canonicalMapping(slots);
+
+                        if(mapping.second)
+                        {
+                            cout << "Found" << endl;
+                            cout << print(insns, slots) << endl;
+                        }
                     }
                 }
             } while(c_iter.next());
-        } while(bruteforceIterate(constant_slots));
+        } while(cons_iter.next());
 
         // Free the instructions and slots
         for(auto slot: slots)
